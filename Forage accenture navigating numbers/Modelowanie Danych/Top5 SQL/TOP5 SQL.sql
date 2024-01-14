d6f36e9cd8f51f389c21df255f4345af951bac07
mdyczk <143459188+mdyczk@users.noreply.github.com>
2024-01-14 01:52:33 +0100

    Add files via upload

---
+++ Forage accenture navigating numbers/Modelowanie Danych/Top5 SQL/TOP5 SQL.sql
@@ -0,0 +1,41 @@
+-- Usuniêcie cudzys³ów z kategorii 
+UPDATE 
+  Content 
+SET 
+  Category = REPLACE(Category, '"', '')
+
+
+-- Usuniêcie tabeli bez typu reakcji
+DELETE FROM 
+  Reactions 
+WHERE 
+  Type IS NULL
+
+
+-- Wyœwietlenie pierwszych 10 wierszy wyczyszczonej tabeli
+SELECT 
+  TOP 10 r.Content_Id as ID_Kontentu, 
+  r.Type as Typ_Reakcji, 
+  r.Datetime as DataICzas, 
+  c.Type as Typ_Kontentu, 
+  c.Category as Kategoria_Kontentu, 
+  rt.Sentiment as Odczucie, 
+  rt.Score as Wynik 
+FROM 
+  Reactions r 
+  JOIN Content c on c.Content_ID = r.Content_ID 
+  JOIN ReactionTypes rt on rt.Type = r.Type
+
+
+-- Wybranie top 5 kategorii 
+SELECT 
+  TOP 5 c.Category, 
+  SUM(rt.Score) as Wynik 
+FROM 
+  Reactions r 
+  JOIN Content c on c.Content_ID = r.Content_ID 
+  JOIN ReactionTypes rt on rt.Type = r.Type 
+GROUP BY 
+  c.Category 
+ORDER BY 
+  Wynik DESC
